bb7f207962646d002d849edec5e5b3f5bb2266d5
fiq <me@taufiqhdyt.com>
2023-03-07 10:47:18 +0700

    day 1

---
+++ src/routes/day-1/01/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<div class="parent">
+	<div class="child" />
+</div>
+
+<style>
+	@import 'style.css';
+</style>
