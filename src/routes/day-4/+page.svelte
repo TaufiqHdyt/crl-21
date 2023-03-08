31c9bdbdfee1877553f7b917372837b19ff4abe3
fiq <me@taufiqhdyt.com>
2023-03-08 10:37:25 +0700

    day 4

---
+++ src/routes/day-4/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<h2>Keypoints</h2>
+
+<ol>
+	<li>`v` from `vh` & `vw` is viewport; `h` is height, `w` is width</li>
+	<li>value before that is percentage of viewport unit</li>
+	<li>`vmin` & `vmax` for limit the minimal & maximal of viewport value</li>
+</ol>
