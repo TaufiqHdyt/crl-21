623ec2630569c214893fcb9e12e57b35506cc5a9
fiq <me@taufiqhdyt.com>
2023-03-10 10:41:26 +0700

    day 6- review

---
+++ src/routes/day-6/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<h2>Keypoints</h2>
+
+<ol>
+	<li>shouldn't use `em` for fonts</li>
+	<li>use `em` for padding / margin that relates to current element font-size</li>
+  <li>prefer `width: 100%; max-width: fixed-width`</li>
+</ol>
