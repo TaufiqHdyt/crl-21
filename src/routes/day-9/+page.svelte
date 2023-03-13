2c7785f8448431938e54fbc52c855820c03e6aea
fiq <me@taufiqhdyt.com>
2023-03-13 14:47:53 +0700

    reduce the amount of html needed

--- src/routes/day-9/+page.svelte
+++ src/routes/day-9/+page.svelte
@@ -16,8 +16,33 @@
 		</div>
 	</div>
 </div>
-<div class="container">
-	<div id="features" class="row">
+
+<div id="features" class="container flex">
+	<div class="col">
+		<h2>Cheap</h2>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua. Ut enim ad minim veniam.
+		</p>
+	</div>
+	<div class="col">
+		<h2>Quick</h2>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua. Ut enim ad minim veniam.
+		</p>
+	</div>
+	<div class="col">
+		<h2>Affordable</h2>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua. Ut enim ad minim veniam.
+		</p>
+	</div>
+</div>
+
+<div class="outro">
+	<div class="container flex">
 		<div class="col">
 			<h2>Cheap</h2>
 			<p>
@@ -31,34 +56,6 @@
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
 				labore et dolore magna aliqua. Ut enim ad minim veniam.
 			</p>
-		</div>
-		<div class="col">
-			<h2>Affordable</h2>
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-				labore et dolore magna aliqua. Ut enim ad minim veniam.
-			</p>
-		</div>
-	</div>
-</div>
-
-<div class="outro">
-	<div class="container">
-		<div class="row">
-			<div class="col">
-				<h2>Cheap</h2>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-					ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-				</p>
-			</div>
-			<div class="col">
-				<h2>Quick</h2>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-					ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-				</p>
-			</div>
 		</div>
 	</div>
 </div>
