551a1f37af171f22d6075b21b598ce1035520100
fiq <me@taufiqhdyt.com>
2023-03-14 09:54:12 +0700

    day 9 challenge done

--- src/routes/day-9/challenge/+page.svelte
+++ src/routes/day-9/challenge/+page.svelte
@@ -14,6 +14,35 @@
 			</p>
 			<a href="#" class="btn">I want to learn</a>
 		</div>
+		<!-- 2. -->
+		<div class="hero__img">
+			<img src="/hero-img.jpg" alt="website sketches" />
+		</div>
+	</div>
+</div>
+
+<!-- 1. -->
+<div class="content">
+	<div class="container row">
+		<div class="content__text">
+			<h2>Quality designs made custom, on demand, just for you</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+		</div>
+		<div class="content__sidebar flex transpose">
+			<div class="col">
+				<h2>Cheap</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+			</div>
+			<div class="col">
+				<h2>Quick</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+			</div>
+			<div class="col">
+				<h2>Quality</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+			</div>
+		</div>
 	</div>
 </div>
 
