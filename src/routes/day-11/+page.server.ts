31f45e2e2fca5ea676f1ec05b20e8e7efcdbfb29
fiq <me@taufiqhdyt.com>
2023-03-16 09:10:18 +0700

    nav challenge init

--- src/routes/day-11/+page.server.ts
+++ src/routes/day-11/+page.server.ts
@@ -2,6 +2,6 @@ import type { PageServerLoad } from './$types';
 
 export const load = (async () => {
 	return {
-		title: 'Day 11 - A deeper dive into flexbox'
+		title: 'Day 11 - Challenge 3 (Navigation)'
 	};
 }) satisfies PageServerLoad;
