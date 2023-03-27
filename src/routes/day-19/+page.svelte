3378b3bcf64d828fa4ae6b1b03866378d723cc4f
fiq <me@taufiqhdyt.com>
2023-03-27 10:56:35 +0700

    day 19 - challenge done

--- src/routes/day-19/+page.svelte
+++ src/routes/day-19/+page.svelte
@@ -6,8 +6,8 @@
 		const navToggle = document.querySelector('.nav-toggle');
 		const nav = document.querySelector('.nav');
 	
-		navToggle.addEventListener('click', () => {
-			nav.classList.toggle('nav--visible');
+		navToggle?.addEventListener('click', () => {
+			nav?.classList.toggle('nav--visible');
 		});
 	}
 
